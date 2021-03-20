0c2f9c4546ba69386c266ae46c21e3fbf947aab7
danielpimentel00 <danielpimentel19@gmail.com>
2021-03-20 18:19:06 -0400

    Ship movement
    up and down ship movement

---
+++ Android Project/Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovement : MonoBehaviour
+{
+    float moveSpeed = 0.12f;
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MoveUpDown();
+    }
+
+    void MoveUpDown()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(touchPos.y > 0 && touchPos.x > 1)
+            {
+                //move up
+                transform.Translate(0, moveSpeed, 0);
+            }
+            else if(touchPos.y < 0 && touchPos.x > 1)
+            {
+                //move down
+                transform.Translate(0, -moveSpeed, 0);
+            }
+        }
+    }
+}
